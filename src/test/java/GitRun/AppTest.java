d94e38a78544bf01de2746b7f362cff1c79a975f
asifalam2022 <109353587+asifalam2022@users.noreply.github.com>
2024-05-27 11:22:28 +0530

    1st push
    1st push

---
+++ src/test/java/GitRun/AppTest.java
@@ -0,0 +1,22 @@
+package GitRun;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+               
+        System.out.println("Test File Executed");
+        
+    }
+}
